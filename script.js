e4f3e360418a728ff37cd3b5bcc20cba8869851f
Mohan Kumar <MohanKumarYum@Gmail.com>
2018-10-30 18:34:34 +1100

    Updated variable names.

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ var temperatureInCelsius = 40;
 
 var celsiusToFahrenheit = (temperatureInCelsius * 9) / 5 + 32
 
-var fahrenheitToCelsius = ((celsiusToFahrenheit - 32) * 5) / 9;
-
 console.log("Temperature of " + temperatureInCelsius + " celsius, will be " + celsiusToFahrenheit + " fahrenheit");
-console.log("Temperature of " + celsiusToFahrenheit + " fahrenheit, will be " + fahrenheitToCelsius + " celsius");
+
+var temperatureInFahrenheit = 104;
+
+var fahrenheitToCelsius = ((temperatureInFahrenheit - 32) * 5) / 9;
+
+console.log("Temperature of " + temperatureInFahrenheit + " fahrenheit, will be " + fahrenheitToCelsius + " celsius");
